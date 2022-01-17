9939b946a4ca503a3d4076459086be644c05527f
raulfranck <raulengfranck@gmail.com>
2022-01-16 21:40:55 -0300

    user tests

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+import {ShowUserProfileError} from './ShowUserProfileError'
+
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Should be able to show user profile", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(
+      usersRepositoryInMemory,
+    );
+    showUserProfileUseCase = new ShowUserProfileUseCase(
+      usersRepositoryInMemory,
+    );
+  });
+  it("Should be possible to show a user's profile", async () => {
+    const userDTO = {
+      name: "Raul Franck",
+      email: "raulengranck@gmail.com",
+      password: "123senha",
+    };
+    const user = await createUserUseCase.execute(userDTO);
+
+    const findUser = await showUserProfileUseCase.execute(user.id || "test")
+    expect(findUser.name).toBe("Raul Franck");
+  });
+
+  it("Shouldn't be able to show the profile of a non-existent user", async () => {
+    expect(async () => {
+      const userDTO = {
+        name: "Raul Franck",
+        email: "raulengranck@gmail.com",
+        password: "123senha",
+      };
+      const user = await createUserUseCase.execute(userDTO);
+      await showUserProfileUseCase.execute("fakeid");
+    }).rejects.toBeInstanceOf(ShowUserProfileError);
+  });
+});
